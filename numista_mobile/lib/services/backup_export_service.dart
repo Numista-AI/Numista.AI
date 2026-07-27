3027026602e23b2d46694a138023e3a28c95e001
Numista Deployer <deploy@numista.ai>
2026-07-27 09:45:46 -0400

    fix(build): move _showCreateAccountDialog to class level; add dart:js_interop for toJS extension

--- numista_mobile/lib/services/backup_export_service.dart
+++ numista_mobile/lib/services/backup_export_service.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:js_interop';
 import 'package:flutter/foundation.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
